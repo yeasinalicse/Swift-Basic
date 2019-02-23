d9fbc5870c88916d187a693ce0062111b6b44d36
yeasinalicse <yeasinalicse@gmail.com>
2019-02-23 14:52:16 +0600

    String loof and reversed

---
+++ StringLoop.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var animal = "yeasin"
+
+//var animalrev = animal.characters
+
+//var animalrev = animal.characters.reversed()
+var animalrev = animal.characters.reversed()
+for character in animalrev {
+    print ("\(character)")
+}
+
+print (animal.count)
